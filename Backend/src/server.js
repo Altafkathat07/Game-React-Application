092e4bdee1b65a2359ab0013e319b032a99ce064
AltafKathathub2 <141987169+AltafKathathub2@users.noreply.github.com>
2024-07-16 17:00:34 +0530

    remove project code

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,36 +1,3 @@
-import express from 'express';
-import routes from './routes/web';
-import socketIoController from './controllers/socketIoController';
-import cors from 'cors';
-import multer from "multer";
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
-
-require('dotenv').config();
-
-const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-
-const port = process.env.PORT || 8888;
-
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(cors());
-app.use(upload.any());
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(express.json());
-
-
-
-
-routes.initWebRouter(app);
-
-socketIoController.sendMessageAdmin(io);
-
 
 server.listen(port, () => {
     console.log("Connected success port: " + port);
--- Frontend/src/components/AlertMassWrapper.js
+++
@@ -1,9 +0,0 @@
-import Swal from 'sweetalert2';
-
-export const showAlert = (text) => {
-  Swal.fire({
-    text: text,
-    showConfirmButton: false,
-    timer: 2000 
-  });
-};
